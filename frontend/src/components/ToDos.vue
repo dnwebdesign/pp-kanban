0c9201485e1554b84d5f88ad30e6f8ee4931b93c
Luca <luca@dn-webdesign.de>
2022-07-27 15:21:57 +0200

    progress bar bug fixed: there is an own progress bar for each todo-list now

--- frontend/src/components/ToDos.vue
+++ frontend/src/components/ToDos.vue
@@ -4,11 +4,11 @@
         :options="options"
         :value="this.progress"
     ></progress-bar>
-    <ul v-for="todo in todoList.todos" id="todo-list">
+    <ul v-for="todo in todoList.todos" :id="'todolist-' + todoList._id">
       <li>
         <input :id="todo._id" v-model="todo.done" :checked="todo.done"
                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-               type="checkbox" @click="countCheckedTodos">
+               type="checkbox" @click="countCheckedTodos(todoList)">
         <label :for="todo._id" class="ml-2 text-gray-900 dark:text-gray-300">{{ todo.content }}</label>
       </li>
     </ul>
@@ -88,9 +88,10 @@ export default {
             }
           });
     },
-    countCheckedTodos() {
-      let checkboxes = document.querySelectorAll('#todo-list input[type="checkbox"]').length,
-          checked = document.querySelectorAll('#todo-list input[type="checkbox"]:checked').length;
+    countCheckedTodos(todoList) {
+      console.log('#todolist-' + todoList._id + ' input[type="checkbox"]');
+      let checkboxes = document.querySelectorAll('#todolist-' + todoList._id + ' input[type="checkbox"]').length,
+          checked = document.querySelectorAll('#todolist-' + todoList._id + ' input[type="checkbox"]:checked').length;
       console.log("Checkboxen: ", checkboxes);
       console.log("Checked: ", checked);
 
