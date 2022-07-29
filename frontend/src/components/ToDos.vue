5bf8a53ad03b2aa3d60c144b0b2b9a78a2b4ce75
Luca <luca@dn-webdesign.de>
2022-07-29 13:48:25 +0200

    todo edit functionality added to frontend

--- frontend/src/components/ToDos.vue
+++ frontend/src/components/ToDos.vue
@@ -10,6 +10,10 @@
                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                type="checkbox" @click="updateTodo(todoList._id, todo)">
         <label :for="'todolist-'+todo._id" class="ml-2 text-gray-900 dark:text-gray-300">{{ todo.content }}</label>
+        <quick-edit v-model="newTodoValue" buttonOkText="To-Do speichern"
+                    @input="updateTodo(todoList._id, todo)">
+          Editieren
+        </quick-edit>
         <button @click="deleteTodo(todo)">Löschen</button>
       </li>
     </ul>
@@ -98,6 +102,7 @@ export default {
     updateTodo(todoListId, todo) {
       let isChecked = document.querySelector('#todolist-' + todo._id).checked;
       todo.done = isChecked;
+      todo.content = this.newTodoValue;
       axios.patch(updateToDoURL + "/" + todo._id, todo)
           .then(function (response) {
             console.log(response);
